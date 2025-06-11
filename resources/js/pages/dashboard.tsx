c08272d283740306388e0952b33265d0f3f6f5f2
Mohamed Masoud <mo.masoud.7007@gmail.com>
2025-06-11 09:57:53 +0300

    refactor: simplify dashboard layout by removing placeholder patterns
    - Removed PlaceholderPattern components from the dashboard layout to streamline the design.
    - Adjusted the layout structure for improved spacing and visual clarity.
    - Enhanced the overall user experience by refining the dashboard's component arrangement.

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,6 @@
 import { ExpiredPasswordsList } from '@/components/features/dashboard/expired-passwords-list';
 import { ExpiringPasswordsList } from '@/components/features/dashboard/expiring-passwords-list';
 import { RecentPasswordsList } from '@/components/features/dashboard/recent-passwords-list';
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { DashboardPageProps } from '@/types/dashboard';
@@ -28,20 +27,8 @@ export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 lg:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                </div>
-
-                <div className="border-sidebar-border/70 dark:border-sidebar-border mt-4 aspect-video overflow-hidden rounded-xl border p-4">
+            <div className="flex h-full flex-1 flex-col gap-6 divide-y rounded-xl p-4">
+                <div className="border-sidebar-border/70 dark:border-sidebar-border aspect-video overflow-hidden rounded-xl border p-4">
                     {/* First Row: Recently Used (Full Width) */}
                     <RecentPasswordsList passwords={recentPasswords} />
 
